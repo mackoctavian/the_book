c045c0c0f1a813a671d0bc530436722770b0664a
Mack Octavian <maestrooctavian@gmail.com>
2024-06-19 23:48:40 +0300

    Defining a mutexe

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,26 +1,15 @@
-use std::sync::mpsc;
+use std::sync::{mpsc, Mutex};
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
+    let m = Mutex::new(5);
 
-    thread::spawn(move || {
-        let val = vec![
-            String::from("Hello"),
-            String::from("Hi!"),
-            String::from("From"),
-            String::from("Thread"),
-        ];
-
-        for i in val {
-            tx.send(i).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    for received in rx {
-        println!("Got: {received}");
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
     }
+
+    println!("m = {:?}", m);
     
 }
