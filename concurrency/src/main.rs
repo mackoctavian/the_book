06cbdcea87e20e767b31d973565f09b54858a052
Mack Octavian <maestrooctavian@gmail.com>
2024-06-20 00:50:58 +0300

    learned about data sharing between threads using mutexes

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,15 +1,24 @@
-use std::sync::{mpsc, Mutex};
+use std::sync::{Arc, Mutex};
 use std::thread;
-use std::time::Duration;
 
 fn main() {
-    let m = Mutex::new(5);
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = Vec::new();
 
-    {
-        let mut num = m.lock().unwrap();
-        *num = 6;
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn( move || {
+            let mut lock = counter.lock().unwrap();
+            *lock += 1;
+        });
+
+        handles.push(handle);
     }
 
-    println!("m = {:?}", m);
+    for handle in handles {
+        handle.join().unwrap()
+    }
+
+    println!("{}", *counter.lock().unwrap());
     
 }
