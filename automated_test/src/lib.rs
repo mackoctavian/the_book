10280d100b05bb140fc4e9832ee4e983bf8697c8
Mack Octavian <maestrooctavian@gmail.com>
2024-06-20 07:01:23 +0300

    Writing automated test using assert macro

---
+++ automated_test/src/lib.rs
@@ -0,0 +1,40 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 10,
+            height: 5
+        };
+        let smaller = Rectangle {
+            width: 4,
+            height: 2
+        };
+
+        assert!(smaller.can_hold(&larger));
+    }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.height > other.height && self.width > other.width
+    }
+}
